27821eafe516cf48ab7d812e150c0cbdb8d15d13
tnoff <1326564+tnoff@users.noreply.github.com>
2024-07-21 09:34:08 -0700

    Script updates

--- files/backup.sh
+++ files/backup.sh
@@ -5,6 +5,8 @@ set -ux;
 echo "Starting backup script"
 date -u
 
+echo "Getting other variables"
+
 # If deployed within k9s container
 LOCAL_DEPLOY="${LOCAL_DEPLOY:-false}"
 
@@ -20,18 +22,19 @@ else
   OCI_PREFIX="$OCI_PREFIX --config-file $CONFIG_FILE"
 fi
 
-if [ -z "${BUCKET_NAME}" ]; then
-  echo "Bucket name not set, exiting"
-fi
+echo "Using prefix ${OCI_PREFIX}"
 
+echo "Creating backup dir"
 BACKUP_DIR="/opt/backup/files"
 mkdir -p "$BACKUP_DIR"
 
+echo "Generating backup file"
 datetime=$(date -u '+%Y-%m-%d')
 backup_file="$BACKUP_DIR/$datetime.json"
 eval "$DISCORD_PREFIX" "$DISCORD_CONF" db_dump 2>/dev/null > "$backup_file"
-gzip "$backup_file"
+gzip "$backup_file" --force
 
+echo "Uploading backup file"
 eval "$OCI_PREFIX" os object put -bn "$BUCKET_NAME" --file "$backup_file.gz" --name "$backup_file.gz" --force
 
 find "$BACKUP_DIR" -type f -mtime +30 -delete
